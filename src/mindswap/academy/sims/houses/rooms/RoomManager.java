f56cbe373c94a3f0853c0b5a11d05ee824aeaea8
Gonçalo <goncaloscravo@gmail.com>
2023-07-26 16:15:01 +0100

    add RoomManager implementation with command pattern !

--- src/mindswap/academy/sims/houses/rooms/RoomManager.java
+++ src/mindswap/academy/sims/houses/rooms/RoomManager.java
@@ -2,6 +2,18 @@ package mindswap.academy.sims.houses.rooms;
 
 public enum RoomManager {
 
+    BEDROOM(new Bedroom()),
+    KITCHEN(new Kitchen()),
+    LIVING(new Living()),
+    OFFICE(new Office());
 
+    private final RoomHandler roomHandler;
 
+    RoomManager(RoomHandler roomHandler) {
+        this.roomHandler = roomHandler;
+    }
+
+    public RoomHandler getRoomHandler() {
+        return roomHandler;
+    }
 }
