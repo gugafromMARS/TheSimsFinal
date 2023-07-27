13be2f25f0632b8e72e5694b7e995499550743af
Gonçalo <goncaloscravo@gmail.com>
2023-07-27 11:20:22 +0100

    changing stats from player when he did something on the room and decrease cleanliness on house and in that room!

--- src/mindswap/academy/sims/houses/rooms/Bedroom.java
+++ src/mindswap/academy/sims/houses/rooms/Bedroom.java
@@ -4,17 +4,45 @@ import mindswap.academy.sims.messages.Messages;
 import mindswap.academy.sims.player.SimsChar;
 
 public class Bedroom implements Room {
+
+    private int maxLevelOfCleanliness;
+    private int levelOfCleanliness;
+
+    public Bedroom() {
+        maxLevelOfCleanliness = 100;
+        levelOfCleanliness = 100;
+    }
+
     @Override
     public void execute(SimsChar simsChar) {
-        simsChar.getPh().sendMessage(Messages.SLEEPING);
+        if(simsChar.getHouse().getLevelOfCleanness() >= 100 && getLevelOfCleanliness() >= 20){
+            simsChar.setEnergyLevel(100);
+            simsChar.getPh().sendMessage(Messages.SLEEPING);
+            decreaseCleanness(50);
+            simsChar.getHouse().decreaseCleanness(20);
+            return;
+        }
+        simsChar.getPh().sendMessage(Messages.YOU_CANT_USE_ROOM);
     }
     @Override
     public void setLevelOfCleanliness(int cleanliness) {
-
+            levelOfCleanliness = cleanliness;
     }
 
     @Override
     public int getLevelOfCleanliness() {
-        return 0;
+        return levelOfCleanliness;
     }
+
+    @Override
+    public void decreaseCleanness(int levelOfDirt) {
+        levelOfCleanliness -= levelOfDirt;
+    }
+
+    @Override
+    public void cleanRoom() {
+        setLevelOfCleanliness(maxLevelOfCleanliness);
+    }
+
+
 }
--- src/mindswap/academy/sims/houses/rooms/Living.java
+++ src/mindswap/academy/sims/houses/rooms/Living.java
@@ -4,18 +4,47 @@ import mindswap.academy.sims.messages.Messages;
 import mindswap.academy.sims.player.SimsChar;
 
 public class Living implements Room {
+
+    private int maxLevelOfCleanliness;
+    private int levelOfCleanliness;
+
+    public Living() {
+        maxLevelOfCleanliness = 100;
+        levelOfCleanliness = 100;
+    }
     @Override
     public void execute(SimsChar simsChar) {
-        simsChar.getPh().sendMessage(Messages.TV);
+        if(simsChar.getEnergyLevel() >= 20){
+            if(simsChar.getHouse().getLevelOfCleanness() >= 100 && getLevelOfCleanliness() >= 20){
+                simsChar.consumeEnergy(20);
+                simsChar.getPh().sendMessage(Messages.TV);
+                decreaseCleanness(50);
+                simsChar.getHouse().decreaseCleanness(20);
+                return;
+            }
+            simsChar.getPh().sendMessage(Messages.YOU_CANT_USE_ROOM);
+            return;
+        }
+        simsChar.getPh().sendMessage(Messages.YOU_NEED_TO_REST);
     }
 
     @Override
     public void setLevelOfCleanliness(int cleanliness) {
-
+        levelOfCleanliness = cleanliness;
     }
 
     @Override
     public int getLevelOfCleanliness() {
-        return 0;
+        return levelOfCleanliness;
+    }
+
+    @Override
+    public void decreaseCleanness(int levelOfDirt) {
+        levelOfCleanliness -= levelOfDirt;
+    }
+
+    @Override
+    public void cleanRoom() {
+        setLevelOfCleanliness(maxLevelOfCleanliness);
     }
 }
